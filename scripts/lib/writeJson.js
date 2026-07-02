bdd819c44d3e86bdb4fae78cd357b424eb55669e
toty <samuelmbarbosa1107@gmail.com>
2026-07-01 21:01:13 -0300

    fix: create public directory before exporting collections

--- scripts/lib/writeJson.js
+++ scripts/lib/writeJson.js
@@ -1,6 +1,11 @@
 import fs from "fs/promises";
+import { dirname } from "path";
 
 export async function writeJson(path, data) {
+	await fs.mkdir(dirname(path), {
+		recursive: true,
+	});
+
 	await fs.writeFile(
 		path,
 		JSON.stringify(data, null, 2),
